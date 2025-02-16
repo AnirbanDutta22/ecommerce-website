c599f8870088e9d339197baef3bc8087693cba25
Anirban Dutta <meanirbandutta22@gmail.com>
2025-02-17 02:06:48 +0530

    Landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,12 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
+import {
+  HiOutlineHeart,
+  HiOutlineSearch,
+  HiOutlineShoppingBag,
+  HiOutlineUser,
+} from "react-icons/hi";
 
 interface NavItem {
   title: string;
@@ -37,7 +43,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-white shadow-sm">
-      <div className="w-full bg-[#B87159] flex justify-center items-center text-white text-xs p-2.5">
+      <div className="w-full bg-primary flex justify-center items-center text-white text-xs p-2.5">
         FREE SHIPPING* on orders of $50 or more.{" "}
         <Link href="/" className="underline mr-1">
           Carrier surcharge
@@ -45,7 +51,7 @@ const Navbar = () => {
         may apply.
       </div>
       <div className="max-w-screen-2xl mx-auto px-4">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-20">
           <div className="flex-shrink-0">
             <Link href="/">
               <span className="text-2xl font-bold">OLARS</span>
@@ -62,9 +68,36 @@ const Navbar = () => {
               >
                 <Link
                   href={item.path}
-                  className="text-gray-700 hover:text-[#B87159] px-3 py-2"
+                  className="text-gray-700 hover:text-primary px-3 py-2 flex gap-x-2 items-center"
                 >
                   {item.title}
+                  {item.title !== "Contact" && (
+                    <svg
+                      fill="#000000"
+                      height="10px"
+                      width="10px"
+                      version="1.1"
+                      id="Layer_1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      viewBox="0 0 330 330"
+                      xmlSpace="preserve"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        {" "}
+                        <path
+                          id="XMLID_102_"
+                          d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393 c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393 s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
+                        ></path>{" "}
+                      </g>
+                    </svg>
+                  )}
                 </Link>
 
                 <AnimatePresence>
@@ -93,13 +126,34 @@ const Navbar = () => {
           </div>
 
           <div className="flex items-center space-x-4">
+            {/* Profile Icon */}
             <button className="p-2">
-              <span className="sr-only">Search</span>
-              {/* Add search icon */}
+              <HiOutlineUser className="text-gray-700 hover:text-primary w-6 h-6" />
+              <span className="sr-only">Profile</span>
             </button>
-            <button className="p-2">
+
+            {/* Wishlist Icon with Notification */}
+            <button className="p-2 relative">
+              <HiOutlineHeart className="text-gray-700 hover:text-primary w-6 h-6" />
+              <span className="sr-only">Wishlist</span>
+              <span className="absolute top-0 right-0 bg-primary text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
+                3 {/* Replace with dynamic count */}
+              </span>
+            </button>
+
+            {/* Cart Icon with Notification */}
+            <button className="p-2 relative">
+              <HiOutlineShoppingBag className="text-gray-700 hover:text-primary w-6 h-6" />
               <span className="sr-only">Cart</span>
-              {/* Add cart icon */}
+              <span className="absolute top-0 right-0 bg-primary text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
+                5 {/* Replace with dynamic count */}
+              </span>
+            </button>
+
+            {/* Search Icon */}
+            <button className="p-2">
+              <HiOutlineSearch className="text-gray-700 hover:text-primary w-6 h-6" />
+              <span className="sr-only">Search</span>
             </button>
           </div>
         </div>
--- src/components/home/HeroSlider.tsx
+++ src/components/home/HeroSlider.tsx
@@ -30,14 +30,38 @@ const HeroSlider = () => {
   };
 
   return (
-    <div className="relative w-full h-screen bg-gray-50">
+    <div className="relative w-full h-screen bg-[url('/landing_bg.jpg')] bg-cover bg-center bg-no-repeat">
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="w-full max-w-7xl mx-auto px-4 flex">
           <button
             onClick={prevSlide}
-            className="absolute left-4 top-1/2 transform -translate-y-1/2 text-black text-xl z-10"
+            className="absolute left-4 top-1/2 transform -translate-y-1/2 z-10"
           >
-            {"<"}
+            <svg
+              fill="#000000"
+              height="30px"
+              width="30px"
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 330 330"
+              xmlSpace="preserve"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  id="XMLID_92_"
+                  d="M111.213,165.004L250.607,25.607c5.858-5.858,5.858-15.355,0-21.213c-5.858-5.858-15.355-5.858-21.213,0.001 l-150,150.004C76.58,157.211,75,161.026,75,165.004c0,3.979,1.581,7.794,4.394,10.607l150,149.996 C232.322,328.536,236.161,330,240,330s7.678-1.464,10.607-4.394c5.858-5.858,5.858-15.355,0-21.213L111.213,165.004z"
+                ></path>{" "}
+              </g>
+            </svg>
           </button>
 
           <AnimatePresence mode="wait">
@@ -47,19 +71,44 @@ const HeroSlider = () => {
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0.8 }}
               transition={{ duration: 0.5 }}
-              className="bg-white p-12 rounded-lg shadow-sm mx-auto text-center"
+              className="bg-slate-50/70 p-16 md:p-20 rounded-lg shadow-2xl mx-auto text-center"
             >
-              <p className="text-rose-400 font-script text-2xl mb-4">
+              <p className="text-primary font-cursive text-3xl md:text-5xl mb-4">
                 {slides[currentSlide].subtitle}
               </p>
-              <h2 className="text-4xl font-bold mb-4">
+              <h2 className="text-5xl md:text-7xl text-black mb-4">
                 {slides[currentSlide].title}
               </h2>
               <p className="text-gray-600 mb-8">
                 {slides[currentSlide].description}
               </p>
-              <button className="bg-rose-700 text-white px-8 py-3 rounded hover:bg-rose-800 transition-colors">
-                {slides[currentSlide].cta}
+              <button className="bg-primary text-white px-5 py-1.5 md:px-8 md:py-3 hover:bg-zinc-900 transition-colors flex items-center gap-x-3 justify-self-center">
+                {slides[currentSlide].cta}{" "}
+                <svg
+                  fill="#ffffff"
+                  height="10px"
+                  width="10px"
+                  version="1.1"
+                  id="Layer_1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 330 330"
+                  xmlSpace="preserve"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      id="XMLID_7_"
+                      d="M288.107,116.894c-5.858-5.858-15.355-5.858-21.213,0c-5.858,5.858-5.858,15.355,0,21.213L278.787,150H15 c-8.284,0-15,6.716-15,15c0,8.284,6.716,15,15,15h263.787l-11.894,11.893c-5.858,5.858-5.858,15.355,0,21.213 c2.929,2.929,6.768,4.394,10.606,4.394c3.839,0,7.678-1.464,10.607-4.394l37.5-37.5c5.858-5.858,5.858-15.355,0-21.213 L288.107,116.894z"
+                    ></path>{" "}
+                  </g>
+                </svg>
               </button>
             </motion.div>
           </AnimatePresence>
@@ -68,7 +117,31 @@ const HeroSlider = () => {
             onClick={nextSlide}
             className="absolute right-4 top-1/2 transform -translate-y-1/2 text-black text-xl z-10"
           >
-            {">"}
+            <svg
+              fill="#000000"
+              height="30px"
+              width="30px"
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 330 330"
+              xmlSpace="preserve"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  id="XMLID_92_"
+                  d="M218.787,165.004L79.393,25.607c-5.858-5.858-5.858-15.355,0-21.213c5.858-5.858,15.355-5.858,21.213,0.001 l150,150.004C253.42,157.211,255,161.026,255,165.004c0,3.979-1.581,7.794-4.394,10.607l-150,149.996 C97.678,328.536,93.839,330,90,330s-7.678-1.464-10.607-4.394c-5.858-5.858-5.858-15.355,0-21.213L218.787,165.004z"
+                ></path>{" "}
+              </g>
+            </svg>
           </button>
         </div>
       </div>
